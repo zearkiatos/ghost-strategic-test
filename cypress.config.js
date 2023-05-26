3a627ef5b7aeaf67aed6b8db20a75e5dec76c810
migueltovar <loganitsolutions@outlook.com>
2023-05-25 21:35:21 -0500

    implementando el smart monkey

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,25 @@
 module.exports = {
+  projectId: 'ghost-strategic-test',
+  env: {
+    appName: 'Ghost CMS',
+    events:1000,
+    delay:300,
+    seed:1234,
+    pctClicks:12,
+    pctScroll:12,
+    pctSelectors:12,
+    pctKeys:12,
+    pctSpKeys:12,
+    pctPgNav:12,
+    pctBwChaos:12,
+    pctActions:16,
+  },
+  pageLoadTimeout: 120000,
+  defaultCommandTimeout: 120000,
+  videosFolder: './results',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return require('./cypress/plugins/index.js')(on, config)
     },
   },
 };
