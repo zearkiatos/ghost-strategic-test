ff4c2f15822fde1ce001104df53104c1ef27d2af
fcastagnola92 <f.castagnola@uniandes.edu.co>
2023-05-25 22:59:36 -0500

    tests signIn

---
+++ cypress/e2e/e2e-test/test-sigIn-site-apriori-incorrect-username.cy.js
@@ -0,0 +1,39 @@
+
+import SigninPage from "../../pages/SigninPage";
+import { getRandomAccount } from "../../../mock/signin";
+
+
+describe('Escenario iniciar sesión y ver sitio', () => {
+  const signinPage = new SigninPage()
+
+  it('Ingreso fallido debido a correo electrónico incorrecto', () => {
+
+    cy.fixture('login-data.json').then(function (user) {
+      this.user = user;
+      // Given
+      cy.visit(this.user.urlLogin);
+      
+    });
+
+    //obteniendo una cuenta incorrecta
+    const incorrectAccount=getRandomAccount();
+
+
+    cy.fixture('login-data.json').then(function (user) {
+      this.user = user;
+
+      // When
+      signinPage.ingresarCorreoElectronico(incorrectAccount.email);
+      
+      signinPage.ingresarPassword(this.user.contraseña)
+      signinPage.hacerClicEnIniciarSesion()
+      
+
+      // Then
+      cy.contains('There is no user with that email address').should('exist')
+       
+      
+    }); 
+  });
+
+})
